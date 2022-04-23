b722d8fd3af6d459450c7e51d119c4d3c796bc72
Nabeel Ahmed <l190916@lhr.nu.edu.pk>
2022-04-23 19:54:21 +0500

    Login/Register backend functionality implemented

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -21,7 +21,6 @@ const authUser = asyncHandler(async (req, res) => {
     } else {
         res.status(401).json({ message: "Invalid Email or Password" })
     }
-
 })
 
 
@@ -29,34 +28,58 @@ const authUser = asyncHandler(async (req, res) => {
 //@route  GET /api/users/profile
 //@access Private
 const getUserProfile = asyncHandler(async (req, res) => {
-
+    const user = await User.findById(req.user._id)
+    console.log(user)
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.nane,
+            email: user.email,
+        })
+    }
+    else {
+        res.status(404).json({ message: "User not found" })
+    }
     res.json({ message: 'Success' })
 })
 
 //@desc   Get users
 //@route  Get /api/products
 //@access Public
-const getUsers = asyncHandler(async (req, res) => {
-    const users = await User.find({})
-    res.json(users)
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, age, password, weight, email, height, image } = req.body
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+        res.status(400).json({ message: "User already exists" })
+    }
+
+    const user = await User.create({
+        name,
+        age,
+        password,
+        weight,
+        email,
+        height,
+        image
+    })
+
+    if (user) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.nane,
+            email: user.email,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid User Data')
+    }
 })
 
-//@desc   Get user by ID
-//@route  Get /api/products/:id
-//@access Public
-const getUserById = asyncHandler(async (req, res) => {
-    const user = await User.findById(req.params.id)
-    if (user) {
-        res.json(user)
-    }
-    else {
-        res.status(404).json({ message: "User not found " })
-    }
-})
 
 export {
     authUser,
-    getUsers,
-    getUserById,
+    registerUser,
     getUserProfile
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,18 +1,13 @@
 import express from "express";
 const router = express.Router()
-import { authUser, getUsers, getUserById, getUserProfile } from "../controllers/userController.js"
-
+import { authUser, registerUser, getUserProfile } from "../controllers/userController.js"
+import { protect } from '../middleware/authMiddleware.js'
 
 
 
 
 router.post('/login', authUser)
-router.route('/profile').get(getUserProfile)
-
-router.route('/').get(getUsers)
-
-router.route('/:id').get(getUserById)
-
-
+router.route('/profile').get(protect, getUserProfile)
+router.route('/').post(registerUser)
 
 export default router
