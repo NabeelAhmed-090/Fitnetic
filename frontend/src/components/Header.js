515b0ab0e7627aa0116ef4ddbfbd6eaabab9cd8c
Nabeel Ahmed <l190916@lhr.nu.edu.pk>
2022-05-11 02:56:58 +0500

    local changes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useSelector } from 'react-redux'
 import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap"
 import { useDispatch } from 'react-redux'
