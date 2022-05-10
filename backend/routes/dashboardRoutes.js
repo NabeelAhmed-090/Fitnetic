82896d679821acc56c8b026b6aa9f8344d2506de
laibaarshad03 <laibaarshad452@gmail.com>
2022-05-11 02:56:04 +0500

    ded

--- backend/routes/dashboardRoutes.js
+++ backend/routes/dashboardRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { getQuestions } from "../controllers/dashboardController.js";
+import { getQuestions, getAnswers } from "../controllers/dashboardController.js";
 const router = express.Router()
 
 
 router.get('/', getQuestions)
-
+router.get('/answers/:id', getAnswers)
 
 export default router
