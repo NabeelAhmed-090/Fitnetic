e5c71e768debb753652f9afedf87f5bf0e931884
Nabeel Ahmed <l190916@lhr.nu.edu.pk>
2022-05-11 21:47:55 +0500

    Local save

--- backend/controllers/dashboardController.js
+++ backend/controllers/dashboardController.js
@@ -4,29 +4,30 @@ import { ObjectId } from 'mongodb';
 import mongoose from 'mongoose';
 
 const getQuestions = asyncHandler(async (req, res) => {
-    const data=await Dashboard.find()
-    
-    const questions=data.map((i)=>{
-        return{
-            questions:i.questions,
-            _id:i._id
-        } 
+    const data = await Dashboard.find()
+
+    const questions = data.map((i) => {
+        return {
+            questions: i.questions,
+            _id: i._id
+        }
     })
     res.json(
-       questions
+        questions
     )
 })
 
 const getAnswers = asyncHandler(async (req, res) => {
-    
-    var id=req.params.id;
-    id=id.slice(0,-1);
-    const _id=ObjectId(id)
-    const data=await Dashboard.findById(_id)
-    const {answer}=data
+    var id = req.params.id;
+    if (id.length == 25) {
+        id = id.slice(0, -1);
+    }
+    const _id = ObjectId(id)
+    const data = await Dashboard.findById(_id)
+    const { answer } = data
     res.json(
         answer
     )
 })
 
-export {getQuestions, getAnswers}
+export { getQuestions, getAnswers }
