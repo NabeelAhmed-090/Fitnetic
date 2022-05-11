393c8c086011f4814b856794c8ee50a173bbfd9d
ayesharizwann <ayesha.rixwan@gmail.com>
2022-05-11 13:14:22 -0700

    post and filter question functionality implemented

--- frontend/src/screens/Dashboard/DashboardAnswers.js
+++ frontend/src/screens/Dashboard/DashboardAnswers.js
@@ -1,14 +1,14 @@
-import React,{useState,useEffect} from 'react'
-import axios from "axios"
-import {useParams} from "react-router-dom"
+import React,{useState,useEffect} from 'react';
+import axios from "axios";
+import {useParams} from "react-router-dom";
 
 
 const DashboardAnswers = () => {
     const [answers, setAnswers] = useState([])
+    const {id} = useParams()
     useEffect(() => {
-        async function getAnswers() {
-            const {id} = useParams()
-            const result = await axios.get(`http://localhost:3000/api/dashboard/answers/${id}`)
+        async function getAnswers() { 
+            const result = await axios.get(`/api/dashboard/answers/${id}`)
             const { data } = result
             setAnswers(data)
         }
@@ -18,7 +18,7 @@ const DashboardAnswers = () => {
     <div>
         {
         answers.map((i) => {
-                    return <h1>i</h1>
+                    return <h1 key={i}>{i}</h1>
                 })
         }
     </div>
